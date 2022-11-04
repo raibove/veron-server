14df11128f01dbbecd44673641faa44ba13071ff
Shweta Kale <shwetakale144@gmail.com>
2022-11-04 17:41:00 +0530

    update add to cart logic

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -5,60 +5,82 @@ import { OAuth2Client } from "google-auth-library";
 const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 export const addToCart = async (req: Request, res: Response) => {
-  const data = req.body;
-  let findProduct = { userMail: data.userMail, productId: data.productId };
-  let userProduct = await Cart.findOne(findProduct);
-  if (userProduct) {
-    let updatedQuantity = userProduct.quantity + 1;
-    Cart.findOneAndUpdate(
-      findProduct,
-      { quantity: updatedQuantity },
-      { new: true },
-      (err, doc) => {
-        if (err) {
-          return res.status(500).json("Something wrong when updating data!");
-        }
-      }
-    );
-  } else {
-    // add product
-    let newUserProduct = await new Cart({
-      userMail: data.userMail,
-      name: data.name,
-      productId: data.productId,
-      price: data.productId,
-      quantity: data.quantity,
-      productImage: data.productImage,
-      category: data.category,
-    });
-    await newUserProduct.save();
+  const bearerHeader = req.headers["authorization"];
+
+  if (!bearerHeader) {
+    return res.status(403).json("Not Authorized");
   }
-  return res.status(200).json("product added to cart");
+
+  try {
+    const bearer = bearerHeader?.split(" ");
+    const token = bearer[1];
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    if (!payload) {
+      return res.status(403).json("User do not exist");
+    }
+
+    const data = req.body;
+    let findProduct = { userMail: payload.email, productId: data.productId };
+    let userProduct = await Cart.findOne(findProduct);
+    if (userProduct) {
+      let updatedQuantity = userProduct.quantity + 1;
+      Cart.findOneAndUpdate(
+        findProduct,
+        { quantity: updatedQuantity },
+        { new: true },
+        (err, doc) => {
+          if (err) {
+            return res.status(500).json("Something wrong when updating data!");
+          }
+        }
+      );
+    } else {
+      // add product
+      let newUserProduct = await new Cart({
+        userMail: data.userMail,
+        name: data.name,
+        productId: data.productId,
+        price: data.productId,
+        quantity: data.quantity,
+        productImage: data.productImage,
+        category: data.category,
+      });
+      await newUserProduct.save();
+    }
+    return res.status(200).json("product added to cart");
+  } catch (e) {
+    res.status(403).json("Failed to validate user");
+  }
 };
 
 export const getCart = async (req: Request, res: Response) => {
   const bearerHeader = req.headers["authorization"];
 
   if (!bearerHeader) {
-    return res.status(403).json("not authorized");
+    return res.status(403).json("Not Authorized");
   }
-  try{
-  const bearer = bearerHeader?.split(" ");
-  const token = bearer[1];
-  const ticket = await googleClient.verifyIdToken({
-    idToken: token,
-    audience: process.env.GOOGLE_CLIENT_ID,
-  });
+  try {
+    const bearer = bearerHeader?.split(" ");
+    const token = bearer[1];
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
 
-  const payload = ticket.getPayload();
-  if (!payload) {
-    return res.status(403).json("User do not exist");
-  }
+    const payload = ticket.getPayload();
+    if (!payload) {
+      return res.status(403).json("User do not exist");
+    }
 
-  let userCart = await Cart.find({ userMail: payload.email });
+    let userCart = await Cart.find({ userMail: payload.email });
 
-  return res.json(userCart);}
-  catch(e){
-    res.status(403).json("Failed to validate user")
+    return res.json(userCart);
+  } catch (e) {
+    res.status(403).json("Failed to validate user");
   }
 };
