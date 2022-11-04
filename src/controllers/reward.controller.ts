97866ae6420a46812ec9f1c53db61e97c69797a8
Shweta Kale <shwetakale144@gmail.com>
2022-11-05 02:34:09 +0530

    Update reward logic

--- src/controllers/reward.controller.ts
+++ src/controllers/reward.controller.ts
@@ -26,7 +26,7 @@ export const getPoints = async (req: Request, res: Response) => {
     if (!user) {
       return res.status(200).json({ points: 0 });
     }
-    return res.status(200).json({ points: 10 });
+    return res.status(200).json({ points: user.points });
   } catch (e) {
     res.status(403).json("Failed to validate user");
   }
@@ -49,14 +49,13 @@ export const addPoints = async (req: Request, res: Response) => {
     if (!payload) {
       return res.status(403).json("User do not exist");
     }
-
     let user = await Reward.findOne({ userMail: payload?.email });
     if (user) {
-        let currentPoints = user.points + req.body.points
+      let currentPoints = user.points + req.body.points;
 
-        await Reward.findOneAndUpdate(
+      Reward.findOneAndUpdate(
         { userMail: payload?.email },
-        { points: currentPoints},
+        { points: currentPoints },
         { new: true },
         (err, doc) => {
           if (err) {
@@ -73,6 +72,51 @@ export const addPoints = async (req: Request, res: Response) => {
       await user.save();
     }
     return res.status(200).json("Points Added");
+  } catch (e) {
+    res.status(403).json("Failed to validate user");
+  }
+};
+
+export const claimReward = async (req: Request, res: Response) => {
+  const bearerHeader = req.headers["authorization"];
+  if (!bearerHeader) {
+    return res.status(403).json("Not Authorized");
+  }
+  try {
+    const bearer = bearerHeader?.split(" ");
+    const token = bearer[1];
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    if (!payload) {
+      return res.status(403).json("User do not exist");
+    }
+    let user = await Reward.findOne({ userMail: payload?.email });
+    if (user) {
+      let currentPoints = user.points - req.body.points;
+
+     Reward.findOneAndUpdate(
+        { userMail: payload?.email },
+        { points: currentPoints },
+        { new: true },
+        (err, doc) => {
+          if (err) {
+            return res.status(500).json("Something wrong when updating data!");
+          }
+        }
+      );
+    } else {
+      user = new Reward({
+        userMail: payload?.email,
+        points: 0,
+      });
+
+      await user.save();
+    }
+    return res.status(200).json({points: user!.points});
   } catch (e) {
     res.status(403).json("Failed to validate user");
   }
--- src/routes/reward.route.ts
+++ src/routes/reward.route.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { addPoints, getPoints } from "../controllers/reward.controller";
+import { addPoints, claimReward, getPoints } from "../controllers/reward.controller";
 
 const router = express.Router();
 
 router.get("/", getPoints);
 router.post("/", addPoints);
-
+router.post("/claim", claimReward)
 export default router;
